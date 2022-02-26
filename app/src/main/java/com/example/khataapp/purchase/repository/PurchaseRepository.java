86d25221b037a17f8e935623077a8ea7a78c84a2
Umair <ukhan1753@gmail.com>
2022-02-27 04:22:50 +0500

    work from home

--- app/src/main/java/com/example/khataapp/purchase/repository/PurchaseRepository.java
+++ app/src/main/java/com/example/khataapp/purchase/repository/PurchaseRepository.java
@@ -2,11 +2,15 @@ package com.example.khataapp.purchase.repository;
 
 import static com.example.khataapp.utils.CONSTANTS.GET_DEPARTMENT;
 import static com.example.khataapp.utils.CONSTANTS.GET_PURCHASES;
+import static com.example.khataapp.utils.CONSTANTS.GET_SUPPLIER;
 import static com.example.khataapp.utils.CONSTANTS.SERVER_ERROR;
+
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.example.khataapp.Interface.CallBackListener;
+import com.example.khataapp.models.GetPartyServerResponse;
 import com.example.khataapp.models.GetPurchaseResponse;
 import com.example.khataapp.network.ApiClient;
 
@@ -70,5 +74,64 @@ public class PurchaseRepository {
 
             }
         });
+    }
+
+
+    public void getPartiesFromServer(String type,String businessID) {
+
+
+
+        Call<GetPartyServerResponse> call = ApiClient.getInstance().getApi().getParties(businessID,type);
+        call.enqueue(new Callback<GetPartyServerResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<GetPartyServerResponse> call, @NonNull Response<GetPartyServerResponse> response) {
+                if (response.isSuccessful())
+                {
+                    if (response.body()!=null)
+                    {
+                        if (response.body().getCode()==200)
+                        {
+                            if (callBackListener!=null)
+                            {
+                                callBackListener.getServerResponse(response.body(), GET_SUPPLIER);
+
+                            }
+                        }
+                        else
+                        {
+                            callBackListener.getServerResponse(response.body().getMessage(),SERVER_ERROR);
+
+                        }
+
+                    }
+                    else
+                    {
+                        callBackListener.getServerResponse(response.message(),SERVER_ERROR);
+
+                    }
+                }
+                else
+                {
+                    if (response.errorBody() != null) {
+                        if (callBackListener!=null)
+                        {
+                            callBackListener.getServerResponse(response.errorBody().toString(),SERVER_ERROR);
+
+                        }
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<GetPartyServerResponse> call, @NonNull Throwable t) {
+                Log.e("Parties Saving Error:",t.getMessage());
+                callBackListener.getServerResponse(t.getMessage(),SERVER_ERROR);
+
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/khataapp/purchase/viewmodel/PurchaseViewModel.java
+++ app/src/main/java/com/example/khataapp/purchase/viewmodel/PurchaseViewModel.java
@@ -1,30 +1,52 @@
 package com.example.khataapp.purchase.viewmodel;
 
+import static com.example.khataapp.utils.CONSTANTS.GET_SUPPLIER;
+import static com.example.khataapp.utils.CONSTANTS.SERVER_ERROR;
+
 import android.app.Application;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.ObservableField;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.khataapp.Interface.CallBackListener;
+import com.example.khataapp.models.GetPartyServerResponse;
 import com.example.khataapp.models.Item;
 import com.example.khataapp.models.Party;
 import com.example.khataapp.purchase.adapter.ProductRecyclerAdapter;
+import com.example.khataapp.purchase.repository.PurchaseRepository;
+import com.example.khataapp.utils.SharedPreferenceHelper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class PurchaseViewModel extends AndroidViewModel  {
 
+    private PurchaseRepository repository;
     private final MutableLiveData<Integer> btnAction;
     private final ObservableField<Party> party;
     private final ProductRecyclerAdapter adapter;
+    private final MutableLiveData<String> toastMessage;
+    private final ObservableField<ArrayAdapter<String>> supplierAdapter;
+    private final HashMap<String, String> supplierHashMap;
+    private String supplierCode;
 
 
     public PurchaseViewModel(@NonNull Application application) {
         super(application);
+
+        repository= new PurchaseRepository();
         btnAction= new MutableLiveData<>();
         party = new ObservableField<>();
         adapter = new ProductRecyclerAdapter();
+        toastMessage= new MutableLiveData<>();
+        supplierAdapter= new ObservableField<>();
+        supplierHashMap= new HashMap<>();
+        getSuppliers();
+
     }
 
     public void onClick(int key)
@@ -32,6 +54,14 @@ public class PurchaseViewModel extends AndroidViewModel  {
 
             btnAction.setValue(key);
 
+    }
+
+    public ObservableField<ArrayAdapter<String>> getSupplierAdapter() {
+        return supplierAdapter;
+    }
+
+    public MutableLiveData<String> getToastMessage() {
+        return toastMessage;
     }
 
     public ProductRecyclerAdapter getAdapter() {
@@ -55,5 +85,54 @@ public class PurchaseViewModel extends AndroidViewModel  {
         {
             adapter.setItemList(list);
         }
+    }
+
+
+    public void getSuppliers()
+    {
+        String businessID= SharedPreferenceHelper.getInstance(getApplication()).getBUSINESS_ID();
+
+        repository.getPartiesFromServer("s",businessID);
+
+        getServerResponse();
+    }
+
+    private void getServerResponse() {
+
+        repository.setCallBackListener(new CallBackListener() {
+            @Override
+            public void getServerResponse(Object object, int key) {
+                if (object!=null)
+                {
+                    if (key== GET_SUPPLIER)
+                    {
+                        GetPartyServerResponse partyServerResponse = (GetPartyServerResponse) object;
+                        setUpSupplierSpinner(partyServerResponse.getPartyList());
+
+                    }
+                    else if (key==SERVER_ERROR)
+                    {
+                        toastMessage.setValue((String) object);
+                    }
+                }
+
+            }
+        });
+    }
+
+    private void setUpSupplierSpinner(List<Party> list) {
+
+        List<String> supplierNameList= new ArrayList<>();
+
+        for (Party model:list)
+        {
+            supplierNameList.add(model.getPartyName());
+            supplierHashMap.put(model.getPartyName(), model.getPartyCode());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplication(), android.R.layout.simple_spinner_dropdown_item,supplierNameList);
+
+        supplierAdapter.set(adapter);
+
     }
 }
