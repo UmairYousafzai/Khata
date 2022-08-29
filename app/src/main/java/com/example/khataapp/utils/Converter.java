988b8203c0d772696d7cb758c4223265f2b0a9c6
UmairYousafZai <ukhan1753@gmail.com>
2022-08-30 01:07:50 +0500

    build 1.0.6

--- app/src/main/java/com/example/khataapp/utils/Converter.java
+++ app/src/main/java/com/example/khataapp/utils/Converter.java
@@ -18,6 +18,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import kotlin.jvm.JvmStatic;
+
 public class Converter {
 
     private static String oldNumberString;
